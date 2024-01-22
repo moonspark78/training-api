8204f41226d9b420f525f3752f945b9813d9a4bf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-22 23:53:22 +0100

    now Comments API  :art:

---
+++ src/Components/ApiComments.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+export const ApiComments = () => {
+    const [commentsData, setCommentsData] = useState([]);
+  return (
+    <div>ApiComments</div>
+  )
+}
