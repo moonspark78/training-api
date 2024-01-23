825628ef400c6eec09a0e0bd3bc93d09cb628e02
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-23 21:13:57 +0100

    :rocket: :tada: create function fetching  the AlbumsAPI  :rocket: :tada:

--- src/Components/ApiAlbums.jsx
+++ src/Components/ApiAlbums.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const ApiAlbums = () => {
+    const [comments, setComments] = useState([]);
+
+
+    useEffect(() => {
+        const fetchCommentsData = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/albums"); 
+        }; 
+    },[])
+
+
+
   return (
     <div>
         <h2>ApiAlbums</h2>
