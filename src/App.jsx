9ea1d8cbba789758a30d588c5159d52e9ca7f0de
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-23 17:14:52 +0100

    :rocket: :tada: Create compoenents for the AlbumsAPI  :rocket: :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {ApiUsers} from "./Components/ApiUsers"
 import {ApiComments} from "./Components/ApiComments"
+import {ApiAlbums} from "./Components/ApiAlbums"
 
 function App() {
   return (
     <div className="App">
       {/* <ApiUsers/> */}
-      <ApiComments/>
+      {/* <ApiComments/> */}
+      <ApiAlbums/>
     </div>
   );
 }
---
+++ src/Components/ApiAlbums.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ApiAlbums = () => {
+  return (
+    <div>
+        <h2>ApiAlbums</h2>
+    </div>
+  )
+}
