a98c16b56c7972654cb8035ab047daf4d3d4643a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-24 18:20:59 +0100

    :rocket: :tada: new API Country  :rocket: :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import {ApiUsers} from "./Components/ApiUsers"
 import {ApiComments} from "./Components/ApiComments"
 import {ApiAlbums} from "./Components/ApiAlbums"
+import {ApiCountry} from "./Components/ApiCountry"
 
 function App() {
   return (
     <div className="App">
       {/* <ApiUsers/> */}
       {/* <ApiComments/> */}
-      <ApiAlbums/>
+      {/* <ApiAlbums/> */}
+      <ApiCountry/>
     </div>
   );
 }
---
+++ src/Components/ApiCountry.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+export const ApiCountry = () => {
+    const [countryData, setCountryData]= useState([]);
+
+
+  return (
+    <div>
+    <h3 style={{textDecoration: "underline", color: "blue"}}>ApiCountry</h3>
+    </div>
+  )
+}
