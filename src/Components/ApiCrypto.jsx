2e72f0d11ed53a9ade12d6a4658dbd9b97677833
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-27 19:58:32 +0100

    :rocket: :tada: display on  the screen data of the   APIcrypto  :rocket: :tada:

--- src/Components/ApiCrypto.jsx
+++ src/Components/ApiCrypto.jsx
@@ -5,14 +5,28 @@ export const ApiCrypto = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            
+            const response = await fetch("https://api.coincap.io/v2/assets/");
+            const data = await response.json();
+            console.log(data.data);
+            setCryptoData(data.data)
         };
+        fetchData();
     },[]);
 
 
 
 
   return (
-    <div>ApiCrypto</div>
+    <div>
+        <h3 style={{textDecoration:"underline", color:"red"}}>ApiCrypto</h3>
+        {
+            cryptoData.map(crypto => (
+                <>
+                    <p>{crypto.name}</p>
+                    <a href={crypto.explorer}>Info Sur la Crypto</a>
+                </>
+            ))
+        }
+    </div>
   )
 }
